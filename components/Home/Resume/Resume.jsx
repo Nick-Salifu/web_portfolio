d99267b46c52f72ad7ec0c4c6f039df355e9e144
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-11-07 08:28:42 +0100

    completed the Resume section

---
+++ components/Home/Resume/Resume.jsx
@@ -0,0 +1,34 @@
+import { FaCodepen, FaReact } from "react-icons/fa"
+import ResumeCard from "./ResumeCard"
+import { BsDatabase } from "react-icons/bs"
+import { BiBadge } from "react-icons/bi"
+
+const Resume = () => {
+  return (
+    <div className="pt-20 pb-16">
+        <div className="w-[90%] sm:w-[70%] mx-auto grid grid-cols-1 xl:grid-cols-2 gap-10">
+            {/* Work part */}
+            <div>
+                <h1 className="text-3xl sm:text-4xl font-bold text-white">My Work <span className="text-cyan-200">Experience</span></h1>
+                <div className="mt-10">
+                    <ResumeCard Icon={FaCodepen} role="Frontend Developer" />
+                    <ResumeCard Icon={FaReact} role="Frontend Developer" />
+                    <ResumeCard Icon={BsDatabase} role="Frontend Developer" />
+                </div>
+            </div>
+            
+            {/* EDUCATION SECTION */}
+            <div>
+                <h1 className="text-3xl sm:text-4xl font-bold text-white">My <span className="text-cyan-200">Education</span></h1>
+                <div className="mt-10">
+                    <ResumeCard Icon={BiBadge} role="Code Academy" date="Jan 2020 - Feb 2020" />
+                    <ResumeCard Icon={FaReact} role="Bachelors' Degree in ICT" date=" Feb 2024 - Dec 2025" />
+                    <ResumeCard Icon={BsDatabase} role="Frontend Bootcamp" date="Mar 2025 - Sep 2025" />
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Resume
---
+++ components/Home/Resume/ResumeCard.jsx
@@ -0,0 +1,23 @@
+
+const ResumeCard = ({Icon, role, date}) => {
+  return (
+    <div className="mb-6">
+        <div className="flex items-start space-x-6 bg-blue-950/20 transition-all duration-300 p-4 sm:p-8 rounded-md">
+            <div className="w-10 h-10 sm:w-14 sm:h-14 bg-blue-950 rounded-full flex flex-col items-center justify-center">
+                <Icon className="text-white w-6 h-6 sm:w-8 sm:h-8" />
+            </div>
+            <div className="flex-1">
+                {date && (
+                    <h1 className="mb-2 px-4 py-1 sm:px-6 sm:py-1.5 rounded-full bg-gray-200 text-gray-600 w-fit font-bold text-sm sm:text-lg">
+                        {date}
+                    </h1>
+                )}
+                <h1 className="text-gray-200 text-xl sm:text-2xl font-semibold">{role}</h1>
+                <p className="text-gray-300 text-sm sm:text-base pt-3">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quasi aliquam perferendis </p>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default ResumeCard
