cd77c516b1e6ec39b1f623cfc6816e9d616d7f34
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-11-07 09:22:07 +0100

    completed the projects section

---
+++ components/Home/Projects/Projects.jsx
@@ -0,0 +1,40 @@
+import Image from "next/image"
+
+const Projects = () => {
+  return (
+    <div className="py-16">
+        <h1 className="text-center text-2xl md:text-4xl xl:text-5xl text-white font-bold">
+            A small selection of recent 
+            <span className="text-cyan-300">projects</span>
+        </h1>
+        <div className="w-[70%] mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 mt-16">
+            {/* First Project */}
+            <div>
+                <Image src="/images/p1.jpg" alt="image" width={800} height={650} className="rounded-lg" />
+                <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">Modern Finance Dashboard UI</h1>
+                <h1 className="text-white/80 font-medium pt-2">Apps, UI/UX</h1>
+            </div>
+            {/* Second Project */}
+            <div>
+                <Image src="/images/p2.jpg" alt="image" width={800} height={650} className="rounded-lg" />
+                <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">Portfolio Website for Creatives</h1>
+                <h1 className="text-white/80 font-medium pt-2">Branding, Motion</h1>
+            </div>
+            {/* Third Project */}
+            <div>
+                <Image src="/images/p3.jpg" alt="image" width={800} height={650} className="rounded-lg" />
+                <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">Brand identity for Startups</h1>
+                <h1 className="text-white/80 font-medium pt-2">Branding, UI/UX</h1>
+            </div>
+            {/* Fourth Project */}
+            <div>
+                <Image src="/images/p4.jpg" alt="image" width={800} height={650} className="rounded-lg" />
+                <h1 className="mt-4 text-xl sm:text-2xl font-semibold text-white">SaaS Dashboard Development</h1>
+                <h1 className="text-white/80 font-medium pt-2">App, UI/UX</h1>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Projects
