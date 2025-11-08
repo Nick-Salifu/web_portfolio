911c362fc26db7de1c35efac2d66273c865a6049
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-11-08 12:06:44 +0100

    completed the skills section

---
+++ components/Home/Skills/Skills.jsx
@@ -0,0 +1,62 @@
+"use client"
+
+import { SiJavascript, SiNodedotjs, SiNextdotjs, SiReact, SiTailwindcss, SiTypescript } from "react-icons/si"
+import Tilt from "react-parallax-tilt"
+
+
+
+
+const skills = [
+    {
+        name: 'JavaScript',
+        icon: <SiJavascript />,
+    },
+    {
+        name: 'React.js',
+        icon: <SiReact />,
+    },
+    {
+        name: 'Next.js',
+        icon: <SiNextdotjs />,
+    },
+    {
+        name: 'Node.js',
+        icon: <SiNodedotjs />,
+    },
+    {
+        name: 'Tailwind CSS',
+        icon: <SiTailwindcss />,
+    },
+    {
+        name: 'Typescript',
+        icon: <SiTypescript />,
+    },
+]
+
+
+const Skills = () => {
+  return (
+    <div className="text-white py-16">
+        <h1 className="text-center text-2xl md:text-4xl xl:text-5xl text-white font-bold">
+            My <span className="text-cyan-300">Skills</span>
+        </h1>
+        
+        <div className="flex flex-wrap justify-center mt-6 gap-10">
+            {skills.map((skill) => {
+                return(
+                   <Tilt key={skill.name} scale={1.2} transitionSpeed={400}>
+                        <div className="flex flex-col items-center justify-center bg-[#14134145] text-center w-32 h-38 rounded-3xl">
+                            <div className="text-5xl mb-4 text-gray-300">
+                                {skill.icon}
+                            </div>
+                            <p className="text-purple-400 mt-1">{skill.name}</p>
+                        </div>
+                   </Tilt> 
+                )
+            })}
+        </div>
+    </div>
+  )
+}
+
+export default Skills
