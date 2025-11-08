d6ae93ded6c7548bbc0963a17f568e49bdbb3ef3
Salifu Friday Nicodemus <nicksalifu@gmail.com>
2025-11-08 12:47:20 +0100

    building the client review sction

---
+++ components/Home/ClientReview/ClientReview.jsx
@@ -0,0 +1,47 @@
+"use client"
+
+import Carousel from "react-multi-carousel"
+import "react-multi-carousel/lib/styles.css"
+import ClientReviewCard from "./ClientReviewCard"
+
+ const responsive = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1324 },
+            items: 3,
+            slidesToSlide: 1
+        },
+        tablet: {
+            breakpoint: { max: 1324, min: 764 },
+            items: 2,
+            slidesToSlide: 1
+        },
+        mobile: {
+            breakpoint: { max: 764, min: 0 },
+            items: 1,
+            slidesToSlide: 1
+        }
+    }
+
+const ClientReview = () => {
+  return (
+    <div className="py-16">
+        <h1 className="text-center text-2xl md:text-4xl xl:text-5xl text-white font-bold">
+            Kind works from satisfied <br /> 
+            <span className="text-cyan-200">clients</span>
+        </h1>
+        <div className="mt-16 w-[70%] mx-auto">
+            <Carousel
+                showDots={false}
+                responsive={responsive}
+                infinite={true}
+                autoPlay={true}
+                autoPlaySpeed={4000}
+            >
+                <ClientReviewCard />
+            </Carousel>
+        </div>
+    </div>
+  )
+}
+
+export default ClientReview
---
+++ components/Home/ClientReview/ClientReviewCard.jsx
@@ -0,0 +1,8 @@
+
+const ClientReviewCard = () => {
+  return (
+    <div>ClientReviewCard</div>
+  )
+}
+
+export default ClientReviewCard
